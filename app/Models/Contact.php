d2f892cb3e83cd02c3526649d2294996439bbbc9
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-08-13 11:55:14 +0700

    Contact Model

---
+++ app/Models/Contact.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Contact extends Model
+{
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    protected $table = "contacts";
+    public $incrementing = true;
+    public $timestamps = true;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class, "user_id", "id");
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
@@ -12,4 +13,9 @@ class User extends Model
     protected $keyType = "int";
     public $timestamps = true;
     public $incrementing = true;
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class, "user_id", "id");
+    }
 }
