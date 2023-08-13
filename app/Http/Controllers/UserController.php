f75d9d6ecdbbb343fd50a38dc480fe8ddfb3773e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-08-13 18:07:46 +0700

    Logout User API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -81,4 +81,14 @@ class UserController extends Controller
         $user->save();
         return new UserResource($user);
     }
+
+    public function logout(Request $request): JsonResponse {
+        $user = Auth::user();
+        $user->token = null;
+        $user->save();
+
+        return response()->json([
+            "data" => true
+        ])->setStatusCode(200);
+    }
 }
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -230,4 +230,37 @@ class UserTest extends TestCase
             ]);
     }
 
+    public function testLogoutSuccess()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->delete(uri: '/api/users/logout', headers: [
+            'Authorization' => 'test'
+        ])->assertStatus(200)
+            ->assertJson([
+                "data" => true
+            ]);
+
+        $user = User::where('username', 'test')->first();
+        self::assertNull($user->token);
+
+    }
+
+    public function testLogoutFailed()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->delete(uri: '/api/users/logout', headers: [
+            'Authorization' => 'salah'
+        ])->assertStatus(401)
+            ->assertJson([
+                "errors" => [
+                    "message" => [
+                        "unauthorized"
+                    ]
+                ]
+            ]);
+    }
+
+
 }
